40019ed8aefc75ad837eb860f703c1e1864aa1c0
Aaron Porter and Lucy Berger <aaron.w.porter@gmail.com>
2016-05-23 19:02:18 -0700

    add js and index.html

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("button#hello").click(function() {
+    $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#webpage").prepend("<li>Hello!</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+});
